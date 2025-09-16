adafd5e46e17e2397202b386ca1a28894bde8586
Priyanshu Maurya <priyanshumaurya14286f@gmail.com>
2025-09-16 19:46:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,20 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
+  if (isMenuOpen) {
+    
+    document.body.style.overflow = "hidden";
+  } else {
+    
+    document.body.style.overflow = "";
+  }
+
+  return () => {
+    document.body.style.overflow = "";
+  };
+  }, [isMenuOpen]);
+
+  useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.screenY > 10);
     };
@@ -89,6 +103,7 @@ export const Navbar = () => {
   );
 
 };
+
 
 
 
